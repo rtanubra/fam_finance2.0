0a937381a487556e07f8d09fa7bec3de74352a2b
rtanubra <rdtanubrata@gmail.com>
2019-02-02 14:18:19 -0500

    User capabilities added to project views. Should remove errors from users not being logged in.

--- mysite/categories/views.py
+++ mysite/categories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404,get_list_or_404, redirect
+from django.shortcuts import render, redirect,get_object_or_404,get_list_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 
 #MODELS
@@ -23,172 +23,165 @@ def calculate_summary(categories):
 
 #==================Application level views#==================
 def index(request,person_id,month,year):
-    person = get_object_or_404(People, id=person_id)
-    group = get_object_or_404(Group,id=person.group_couple.id)
-    all_people = get_list_or_404(People,group_couple=group)
-    if len(all_people) > 1:
-        other_members = [ peep for peep in all_people if peep != person ]
+    if request.user.is_authenticated:
+        person = get_object_or_404(People, id=person_id)
+        group = get_object_or_404(Group,id=person.group_couple.id)
+        all_people = get_list_or_404(People,group_couple=group)
+        if len(all_people) > 1:
+            other_members = [ peep for peep in all_people if peep != person ]
+        else:
+            other_members = []
+        if len(person.category_set.filter(category_date__month=month,category_date__year=year))>=1:
+            categories= get_list_or_404(Category,category_person=person,category_date__month=month,category_date__year=year)
+        else:
+            categories = []
+        expected,spent,left = calculate_summary(categories)
+        today = datetime.datetime.today()
+        current_user = get_object_or_404(People, username=request.user)
+        context = {
+            'person':person,
+            'categories':categories,
+            'month':today.month,
+            'year':today.year,
+            'other_members':other_members,
+            'expected': expected,
+            'spent': spent,
+            'left':left,
+            "current_user":current_user
+        }
+        today = datetime.datetime.today()
+        context["this_month"] = today.month
+        context["this_year"] = today.year
+        return render(request, "categories/index.html" ,context)
     else:
-        other_members = []
-    if len(person.category_set.filter(category_date__month=month,category_date__year=year))>=1:
-        categories= get_list_or_404(Category,category_person=person,category_date__month=month,category_date__year=year)
-    else:
-        categories = []
-    expected,spent,left = calculate_summary(categories)
-    today = datetime.datetime.today()
-    current_user = get_object_or_404(People, username=request.user)
-    context = {
-        'person':person,
-        'categories':categories,
-        'month':today.month,
-        'year':today.year,
-        'other_members':other_members,
-        'expected': expected,
-        'spent': spent,
-        'left':left,
-        "current_user":current_user
-    }
-    today = datetime.datetime.today()
-    context["this_month"] = today.month
-    context["this_year"] = today.year
-    return render(request, "categories/index.html" ,context)
+        context = {} 
+        return render(request,'bases/landing_request_login.html',context)
 
 #==================Views here#==================
 def index_team(request,group_id,month,year):
-    group = get_object_or_404(Group,id=group_id)
-    all_people = get_list_or_404(People,group_couple=group)
-    categories = []
-    for person in all_people:
-        if len(person.category_set.filter(category_date__month=month,category_date__year=year))>0:
-            categories.extend(get_list_or_404(Category,category_person=person,category_date__month=month,category_date__year=year))
+    if request.user.is_authenticated:
+        group = get_object_or_404(Group,id=group_id)
+        all_people = get_list_or_404(People,group_couple=group)
+        categories = []
+        for person in all_people:
+            if len(person.category_set.filter(category_date__month=month,category_date__year=year))>0:
+                categories.extend(get_list_or_404(Category,category_person=person,category_date__month=month,category_date__year=year))
 
-    expected,spent,left = calculate_summary(categories)
-    today = datetime.datetime.today()
-    current_user = get_object_or_404(People, username=request.user)
-    context = {
-        'categories':categories,
-        'month':today.month,
-        'year':today.year,
-        'expected': expected,
-        'spent': spent,
-        'left':left,
-        "current_user":current_user
-    }
-    today = datetime.datetime.today()
-    context["this_month"] = today.month
-    context["this_year"] = today.year
-    return render(request, "categories/index_team.html" ,context)
+        expected,spent,left = calculate_summary(categories)
+        today = datetime.datetime.today()
+        current_user = get_object_or_404(People, username=request.user)
+        context = {
+            'categories':categories,
+            'month':today.month,
+            'year':today.year,
+            'expected': expected,
+            'spent': spent,
+            'left':left,
+            "current_user":current_user
+        }
+        today = datetime.datetime.today()
+        context["this_month"] = today.month
+        context["this_year"] = today.year
+        return render(request, "categories/index_team.html" ,context)
+    else:
+        context = {} 
+        return render(request,'bases/landing_request_login.html',context)
 
 def index_person(request,person_id,month,year):
-    person = get_object_or_404(People,id=person_id)
-    if len(person.category_set.filter(category_date__month=month,category_date__year=year))>0:
-        categories = get_list_or_404(Category, category_person=person, category_date__month=month,category_date__year=year)
+    if request.user.is_authenticated:
+        person = get_object_or_404(People,id=person_id)
+        if len(person.category_set.filter(category_date__month=month,category_date__year=year))>0:
+            categories = get_list_or_404(Category, category_person=person, category_date__month=month,category_date__year=year)
+        else:
+            categories = []
+        expected,spent,left = calculate_summary(categories)
+        today = datetime.datetime.today()
+        current_user = get_object_or_404(People, username=request.user)
+        context = {
+            'categories':categories,
+            'month':today.month,
+            'year':today.year,
+            'expected': expected,
+            'spent': spent,
+            'left':left,
+            "current_user":current_user
+        }
+        today = datetime.datetime.today()
+        context["this_month"] = today.month
+        context["this_year"] = today.year
+        return render(request, "categories/index_person.html" ,context)
     else:
-        categories = []
-    expected,spent,left = calculate_summary(categories)
-    today = datetime.datetime.today()
-    current_user = get_object_or_404(People, username=request.user)
-    context = {
-        'categories':categories,
-        'month':today.month,
-        'year':today.year,
-        'expected': expected,
-        'spent': spent,
-        'left':left,
-        "current_user":current_user
-    }
-    today = datetime.datetime.today()
-    context["this_month"] = today.month
-    context["this_year"] = today.year
-    return render(request, "categories/index_person.html" ,context)
+        context = {} 
+        return render(request,'bases/landing_request_login.html',context)
 
 def index_diff_month(request,person_id,month,year):
-    person = get_object_or_404(People, id=person_id)
-    if len(person.category_set.all())>=1:
-        categories= get_list_or_404(Category,category_person=person,category_date__month=month,category_date__year=year)
+    if request.user.is_authenticated:
+        person = get_object_or_404(People, id=person_id)
+        if len(person.category_set.all())>=1:
+            categories= get_list_or_404(Category,category_person=person,category_date__month=month,category_date__year=year)
+        else:
+            categories = []
+        current_user = current_user = get_object_or_404(People, username=request.user)
+        today = datetime.datetime.today()
+        context = {
+            'person':person,
+            'categories':categories,
+            'month':month,
+            'year':year,
+            "current_user":current_user
+        }
+        today = datetime.datetime.today()
+        context["this_month"] = today.month
+        context["this_year"] = today.year
+        return render(request, "categories/index_person.html" ,context)
     else:
-        categories = []
-    current_user = current_user = get_object_or_404(People, username=request.user)
-    today = datetime.datetime.today()
-    context = {
-        'person':person,
-        'categories':categories,
-        'month':month,
-        'year':year,
-        "current_user":current_user
-    }
-    today = datetime.datetime.today()
-    context["this_month"] = today.month
-    context["this_year"] = today.year
+        context = {} 
+        return render(request,'bases/landing_request_login.html',context)
     
 def addCategory(request,person_id):
-    person = get_object_or_404(People, id=person_id)
-    initial_data = {
-        'category_person' : person
-    }
-    form = CategoryAddForm(request.POST or None,initial=initial_data,hide_condition=True)
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            form = CategoryAddForm(initial=initial_data,hide_condition=True)
-        else:
-            print("Something happend!")
-            form = CategoryAddForm(initial=initial_data,hide_condition=True)
-    today = datetime.datetime.today()
-    current_user = current_user = get_object_or_404(People, username=request.user)
-    context={
-        "person":person,
-        "form":form,
-        'month':today.month,
-        'year':today.year,
-        'current_user':current_user
-    }
-    today = datetime.datetime.today()
-    context["this_month"] = today.month
-    context["this_year"] = today.year
-    return render(request,"categories/new_category.html",context)
+    if request.user.is_authenticated:
+        person = get_object_or_404(People, id=person_id)
+        initial_data = {
+            'category_person' : person
+        }
+        form = CategoryAddForm(request.POST or None,initial=initial_data,hide_condition=True)
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                form = CategoryAddForm(initial=initial_data,hide_condition=True)
+            else:
+                print("Something happend!")
+                form = CategoryAddForm(initial=initial_data,hide_condition=True)
+        today = datetime.datetime.today()
+        current_user = current_user = get_object_or_404(People, username=request.user)
+        context={
+            "person":person,
+            "form":form,
+            'month':today.month,
+            'year':today.year,
+            'current_user':current_user
+        }
+        today = datetime.datetime.today()
+        context["this_month"] = today.month
+        context["this_year"] = today.year
+        return render(request,"categories/new_category.html",context)
+    else:
+        context = {} 
+        return render(request,'bases/landing_request_login.html',context)
 
 def editCategory(request,person_id,category_id):
-    person = get_object_or_404(People, id=person_id)
-    category = get_object_or_404(Category, id=category_id)
-    my_instance = category
-    form = CategoryAddForm(request.POST or None,instance=my_instance,hide_condition=True)
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            form = CategoryAddForm(instance=my_instance,hide_condition=True)
-        else:
-            print("Failed validation")
-            form = CategoryAddForm(instance=my_instance,hide_condition=True)
-    today = datetime.datetime.today()
-    current_user = current_user = get_object_or_404(People, username=request.user)
-    context = {
-        'person':person,
-        'category':category,
-        'form':form,
-        'month':today.month,
-        'year':today.year,
-        'current_user':current_user
-    }
-    today = datetime.datetime.today()
-    context["this_month"] = today.month
-    context["this_year"] = today.year
-    return render(request,"categories/edit_category.html",context)
-
-def deleteCategory(request,person_id,category_id):
-    person = get_object_or_404(People, id=person_id)
-    category = get_object_or_404(Category, id=category_id)
-    month = category.category_date.month
-    year = category.category_date.year
-    my_instance = category
-    form = CategoryAddForm(request.POST or None,instance=my_instance,hide_condition=True)
-    if request.method == "POST":
-        if form.is_valid():
-            obj = form.save(commit=False)
-            obj.delete()
-            return redirect(f"../../{month}_{year}")
-    
-    else:
+    if request.user.is_authenticated:
+        person = get_object_or_404(People, id=person_id)
+        category = get_object_or_404(Category, id=category_id)
+        my_instance = category
+        form = CategoryAddForm(request.POST or None,instance=my_instance,hide_condition=True)
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                form = CategoryAddForm(instance=my_instance,hide_condition=True)
+            else:
+                print("Failed validation")
+                form = CategoryAddForm(instance=my_instance,hide_condition=True)
         today = datetime.datetime.today()
         current_user = current_user = get_object_or_404(People, username=request.user)
         context = {
@@ -202,4 +195,40 @@ def deleteCategory(request,person_id,category_id):
         today = datetime.datetime.today()
         context["this_month"] = today.month
         context["this_year"] = today.year
-        return render(request,"categories/delete_category.html",context)
+        return render(request,"categories/edit_category.html",context)
+    else:
+        context = {} 
+        return render(request,'bases/landing_request_login.html',context)
+
+def deleteCategory(request,person_id,category_id):
+    if request.user.is_authenticated:
+        person = get_object_or_404(People, id=person_id)
+        category = get_object_or_404(Category, id=category_id)
+        month = category.category_date.month
+        year = category.category_date.year
+        my_instance = category
+        form = CategoryAddForm(request.POST or None,instance=my_instance,hide_condition=True)
+        if request.method == "POST":
+            if form.is_valid():
+                obj = form.save(commit=False)
+                obj.delete()
+                return redirect(f"../../{month}_{year}")
+    
+        else:
+            today = datetime.datetime.today()
+            current_user = current_user = get_object_or_404(People, username=request.user)
+            context = {
+                'person':person,
+                'category':category,
+                'form':form,
+                'month':today.month,
+                'year':today.year,
+                'current_user':current_user
+            }
+            today = datetime.datetime.today()
+            context["this_month"] = today.month
+            context["this_year"] = today.year
+            return render(request,"categories/delete_category.html",context)
+    else:
+        context = {} 
+        return render(request,'bases/landing_request_login.html',context)
--- mysite/expenses/views.py
+++ mysite/expenses/views.py
@@ -12,74 +12,82 @@ from .forms import ExpenseAddForm
 import datetime
 # Create your views here.
 def add_expense(request,person_id,category_id):
-    person = get_object_or_404(People,id=person_id)
-    category = get_object_or_404(Category,id=category_id)
-    month = category.category_date.month
-    year = category.category_date.year
-    cc = get_object_or_404(Category, category_person=person, category_name="credit_card",category_date__month=month,category_date__year=year)
-    initial_data = {
-        'expense_category':category,
-    }
-    form = ExpenseAddForm(request.POST or None,initial=initial_data,hide_condition=True)
-    if request.method == "POST":
-        if form.is_valid():
-            new_expense = form.save() 
-            #We are paying off credit card
-            if category == cc:
-                category.category_expected -= new_expense.expense_amount
+    if request.user.is_authenticated:
+        person = get_object_or_404(People,id=person_id)
+        category = get_object_or_404(Category,id=category_id)
+        month = category.category_date.month
+        year = category.category_date.year
+        cc = get_object_or_404(Category, category_person=person, category_name="credit_card",category_date__month=month,category_date__year=year)
+        initial_data = {
+            'expense_category':category,
+        }
+        form = ExpenseAddForm(request.POST or None,initial=initial_data,hide_condition=True)
+        if request.method == "POST":
+            if form.is_valid():
+                new_expense = form.save() 
+                #We are paying off credit card
+                if category == cc:
+                    category.category_expected -= new_expense.expense_amount
 
-                category.save()
-            #Purchasing Actual Expense
-            else:
-#==========================pmt type == CC#==========================
-                if new_expense.method_of_payment == "Credit":
-                    cc.category_expected += new_expense.expense_amount
-
-                    category.category_spent += new_expense.expense_amount
-                    cc.save()
                     category.save()
-    #==========================pmt type == Debit/Cash#==========================
+                #Purchasing Actual Expense
                 else:
-                    category.category_spent += new_expense.expense_amount
-                    category.save()
+    #==========================pmt type == CC#==========================
+                    if new_expense.method_of_payment == "Credit":
+                        cc.category_expected += new_expense.expense_amount
+
+                        category.category_spent += new_expense.expense_amount
+                        cc.save()
+                        category.save()
+        #==========================pmt type == Debit/Cash#==========================
+                    else:
+                        category.category_spent += new_expense.expense_amount
+                        category.save()
 
 
-            #Start a new when done.
-            form = ExpenseAddForm(initial=initial_data,hide_condition=True)
-        else:
-            print("HELP")
-            form = ExpenseAddForm(initial=initial_data,hide_condition=True)
-    current_user = current_user = get_object_or_404(People, username=request.user)
-    context = {
-        'form':form,
-        'category':category,
-        'person':person,
-        'month':category.category_date.month,
-        'year':category.category_date.year,
-        'current_user':current_user
-    }
-    today = datetime.datetime.today()
-    context["this_month"] = today.month
-    context["this_year"] = today.year
-    return render(request,'expenses/add_expense.html',context)
+                #Start a new when done.
+                form = ExpenseAddForm(initial=initial_data,hide_condition=True)
+            else:
+                print("HELP")
+                form = ExpenseAddForm(initial=initial_data,hide_condition=True)
+        current_user = current_user = get_object_or_404(People, username=request.user)
+        context = {
+            'form':form,
+            'category':category,
+            'person':person,
+            'month':category.category_date.month,
+            'year':category.category_date.year,
+            'current_user':current_user
+        }
+        today = datetime.datetime.today()
+        context["this_month"] = today.month
+        context["this_year"] = today.year
+        return render(request,'expenses/add_expense.html',context)
+    else:
+        context= {}
+        return render(request,'bases/landing_request_login.html',context)
 
 def category_expense_list(request,person_id,category_id):
-    person = get_object_or_404(People, id=person_id)
-    category = get_object_or_404(Category, id=category_id)
-    if len(category.expense_set.all())>0:
-        expenses = get_list_or_404(Expense,expense_category=category )
+    if request.user.is_authenticated:
+        person = get_object_or_404(People, id=person_id)
+        category = get_object_or_404(Category, id=category_id)
+        if len(category.expense_set.all())>0:
+            expenses = get_list_or_404(Expense,expense_category=category )
+        else:
+            expenses = []
+        current_user = current_user = get_object_or_404(People, username=request.user)
+        context = {
+            'person':person,
+            'category':category,
+            'expenses':expenses,
+            'month':category.category_date.month,
+            'year':category.category_date.year,
+            'current_user':current_user
+        }
+        today = datetime.datetime.today()
+        context["this_month"] = today.month
+        context["this_year"] = today.year
+        return render(request,"expenses/category_expense_detail.html" ,context)
     else:
-        expenses = []
-    current_user = current_user = get_object_or_404(People, username=request.user)
-    context = {
-        'person':person,
-        'category':category,
-        'expenses':expenses,
-        'month':category.category_date.month,
-        'year':category.category_date.year,
-        'current_user':current_user
-    }
-    today = datetime.datetime.today()
-    context["this_month"] = today.month
-    context["this_year"] = today.year
-    return render(request,"expenses/category_expense_detail.html" ,context)
+        context= {}
+        return render(request,'bases/landing_request_login.html',context)
